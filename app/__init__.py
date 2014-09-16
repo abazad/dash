d9d47f4fafe527b1a1d38bacf0d018d43e97bbe9
shikevin <im@shikev.in>
2014-09-16 01:00:38 -0400

    upload button complete, hooking up import backend

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,10 @@
-from flask import Flask
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = set(['txt'])
+
 app = Flask(__name__)
 
 from app import views
