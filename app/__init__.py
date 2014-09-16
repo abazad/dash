1ebb9fb8ceac43862c10853614d70835612a658f
shikevin <im@shikev.in>
2014-09-16 01:00:38 -0400

    added file uploading
    upload button complete, hooking up import backend
    
    still needs lots of cleaning up but the functionality is there

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
-from flask import Flask
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug import secure_filename
+
 app = Flask(__name__)
 
-from app import views
+# from app import views
--- app/views.py
+++ app/views.py
@@ -1,5 +1,43 @@
+import os
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory
+from werkzeug import secure_filename
 from app import app
 
-@app.route('/')
-def index():
-    return 'Hello kevin'
+UPLOAD_FOLDER = 'uploads/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/uploads')
+def hello():
+    return app.config['UPLOAD_FOLDER']
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file))
+            return redirect(url_for('uploaded-file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+    # def index():
+    #     return render_template('index.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
