6f7657b635c10c78b26895a5cc0bcb26f0742d9a
shikevin <im@shikev.in>
2014-09-17 01:03:39 -0400

    still needs lots of cleaning up but the functionality is there

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,6 @@ import os
 from flask import Flask, request, redirect, url_for
 from werkzeug import secure_filename
 
-UPLOAD_FOLDER = '/uploads'
-ALLOWED_EXTENSIONS = set(['txt'])
-
 app = Flask(__name__)
 
-from app import views
+# from app import views
--- run.py
+++ run.py
@@ -1,3 +1,50 @@
 #!flask/bin/python
-from app import app
-app.run(debug = True)
+import os
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory
+from werkzeug import secure_filename
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'app/uploads/'
+NEW_FILENAME = 'upload.txt'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/uploads')
+def hello():
+    return app.config['UPLOAD_FOLDER']
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            # return os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], NEW_FILENAME))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+    # def index():
+    #     return render_template('index.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               NEW_FILENAME)
+
+
+# from app import app
+app.run(debug=True, use_debugger=True,
+        use_reloader=True)
